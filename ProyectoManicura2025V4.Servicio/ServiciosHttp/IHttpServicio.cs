4e9d1604f889fe88df9a8732d0dd97232816dbc6
Marianela Aguirre <maryaguirre297@gmail.com>
2025-10-26 04:05:42 -0300

    crear( error de todo)

--- ProyectoManicura2025V4.Servicio/ServiciosHttp/IHttpServicio.cs
+++ ProyectoManicura2025V4.Servicio/ServiciosHttp/IHttpServicio.cs
@@ -4,5 +4,6 @@ namespace ProyectoManicura2025V4.Servicio.ServiciosHttp
     public interface IHttpServicio
     {
         Task<HttpRespuesta<T>> Get<T>(string url);
+        Task<HttpRespuesta<TResp>> Post<T, TResp>(string url, T entidad);
     }
 }
